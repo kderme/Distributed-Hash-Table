2c7b25ec51003281e664c7194a32add932fea4ab
kostas <kostasder94x@gmail.com>
2017-03-03 02:50:08 +0200

    RoutingServer now uses select (nio.Channel)

--- dht/src/dht/RoutingServer.java
+++ dht/src/dht/RoutingServer.java
@@ -3,9 +3,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -23,8 +31,12 @@ public class RoutingServer extends Thread{
 	private PrintWriter outOne, outNext=null;
 	private BufferedReader inOne;
 
+	  // A pre-allocated buffer for encrypting data
+	  private final ByteBuffer buffer = ByteBuffer.allocate( 16384 );
+	
 	RoutingServer previous,next;
-	protected long start,end;	
+	protected String start,end;
+	protected boolean amiFirst=false;
 	
 	protected RoutingServer(String myIp,int myPort,int k,String oneIp,int onePort){
 		this.myIp=myIp;
@@ -40,7 +52,6 @@ public class RoutingServer extends Thread{
 	 * 		One:	<yourId>-<start>-<end>-<ipNext>:<portNext>
 	 */
 	public void run(){
-		
 		try {
 			socketOne = new Socket(oneIp, onePort);
 			outOne= new PrintWriter(socketOne.getOutputStream(), true);
@@ -74,11 +85,235 @@ public class RoutingServer extends Thread{
 		listen();
 	}
 	
+	private void listen(){
+	    try {
+	        // Instead of creating a ServerSocket,
+	        // create a ServerSocketChannel
+	        ServerSocketChannel ssc = ServerSocketChannel.open();
+
+	        // Set it to non-blocking, so we can use select
+	        ssc.configureBlocking( false );
+
+	        // Get the Socket connected to this channel, and bind it
+	        // to the listening port
+	        ServerSocket ss = ssc.socket();
+	        InetSocketAddress isa = new InetSocketAddress( myPort );
+	        ss.bind( isa );
+
+	        // Create a new Selector for selecting
+	        Selector selector = Selector.open();
+
+	        // Register the ServerSocketChannel, so we can
+	        // listen for incoming connections
+	        ssc.register( selector, SelectionKey.OP_ACCEPT );
+	        System.out.println( "Listening on port "+myPort );
+
+	        while (true) {
+	          // See if we've had any activity -- either
+	          // an incoming connection, or incoming data on an
+	          // existing connection
+	          int num = selector.select();
+
+	          // If we don't have any activity, loop around and wait
+	          // again
+	          if (num == 0) {
+	            System.out.println("num==0");
+	            continue;
+	          }
+
+	          // Get the keys corresponding to the activity
+	          // that has been detected, and process them
+	          // one by one
+	          Set keys = selector.selectedKeys();
+	          Iterator it = keys.iterator();
+	          while (it.hasNext()) {
+	            // Get a key representing one of bits of I/O
+	            // activity
+	            SelectionKey key = (SelectionKey)it.next();
+
+	            // What kind of activity is it?
+	            if ((key.readyOps() & SelectionKey.OP_ACCEPT) ==
+	              SelectionKey.OP_ACCEPT) {
+
+	  System.out.println( "acc" );
+	              // It's an incoming connection.
+	              // Register this socket with the Selector
+	              // so we can listen for input on it
+
+	              Socket s = ss.accept();
+	              System.out.println( "Got connection from "+s );
+
+	              // Make sure to make it non-blocking, so we can
+	              // use a selector on it.
+	              SocketChannel sc = s.getChannel();
+	              sc.configureBlocking( false );
+
+	              // Register it with the selector, for reading
+	              sc.register( selector, SelectionKey.OP_READ );
+	            } else if ((key.readyOps() & SelectionKey.OP_READ) ==
+	              SelectionKey.OP_READ) {
+
+	              SocketChannel sc = null;
+
+	              try {
+
+	                // It's incoming data on a connection, so
+	                // process it
+	                sc = (SocketChannel)key.channel();
+	                boolean ok = processInput( sc );
+
+	                // If the connection is dead, then remove it
+	                // from the selector and close it
+	                if (!ok) {
+	                  key.cancel();
+
+	                  Socket s = null;
+	                  try {
+	                    s = sc.socket();
+	                    s.close();
+	                  } catch( IOException ie ) {
+	                    System.err.println( "Error closing socket "+s+": "+ie );
+	                  }
+	                }
+
+	              } catch( IOException ie ) {
+
+	                // On exception, remove this channel from the selector
+	                key.cancel();
+
+	                try {
+	                  sc.close();
+	                } catch( IOException ie2 ) { System.out.println( ie2 ); }
+
+	                System.out.println( "Closed "+sc );
+	              }
+	            }
+	          }
+
+	          // We remove the selected keys, because we've dealt
+	          // with them.
+	          keys.clear();
+	        }
+	      } catch( IOException ie ) {
+	        System.err.println( ie );
+	      }
+	    }
+		
+	      private boolean processInput( SocketChannel sc ) throws IOException {
+	    	    buffer.clear();
+	    	    sc.read( buffer );
+	    	    buffer.flip();
+
+	    	    // If no data, close the connection
+	    	    if (buffer.limit()==0) {
+	    	      return false;
+	    	    }
+	    	    byte[] bytes=buffer.array();
+	    	    String newMessage=new String(bytes,java.nio.charset.StandardCharsets.UTF_8); //check encoding
+	    	    processMessage(newMessage);
+	    	    sc.write( buffer );
+
+	    	    System.out.println( "Processed "+buffer.limit()+" from "+sc );
+
+	    	    return true;
+	    	  }
+
+	
+	private void listen_old(){
+		try{
+		srvSocket= new ServerSocket(myPort,7); //backlog (max queue)
+		System.out.println("Listening");
+		while(true){
+			Socket sock=srvSocket.accept();
+			System.out.println("Connected");
+			BufferedReader inPrev = new BufferedReader(
+	                new InputStreamReader(sock.getInputStream()));
+			String newMessage=inPrev.readLine();
+			System.out.println(newMessage);
+		
+			processMessage(newMessage);
+			
+					}
+	}	
+	catch(IOException e){
+		e.printStackTrace();
+	}
+	}
+	
 	/*
 	 * This function is called at start or when 
 	 * a routing change happens (someone left or came)
 	 * iPort=<Ip>:<Port>
 	 */
+	
+	protected void processMessage(String newMessage){
+		if(newMessage.startsWith("Leave")){
+			depart(newMessage);
+			// we may not manage to leave
+			return;
+		}
+		
+		if (newMessage.startsWith("Answer-")){
+			//if an answer, comes just print it (?)
+			//	Answer-<answer>
+			System.out.println(newMessage.split("-")[1]);
+			return;
+		}
+
+		if(newMessage.startsWith("One-")){
+			//	Some change happened (someone left or came)
+			//  One-ipNext:portNext
+			try{connectWithNext(newMessage.split("-")[1]);}
+			catch(IOException e){
+				e.printStackTrace();
+				System.out.println("error while connecting with next");
+				depart("LeaveForced");
+			}
+			return;
+		}
+		
+		
+		if(newMessage.startsWith("NewHigh-")){
+			//New Range due to new Node in network
+			String[] ranges=newMessage.split("-");
+			start=ranges[1];
+			amiFirst=(compareHash(start,myShaId)>=0);
+			return;
+		}
+		
+		if(newMessage.startsWith("OneLeft-")){
+			//  TODO : One Died ?
+			return;
+		}
+
+		String sendMessage;
+		if(!newMessage.contains("#")){
+			/* 
+			 * If this is the first hop
+			 * add my ip:port# to take Answer
+			 */
+			sendMessage=myIp+":"+myPort+"#"+newMessage;
+		}
+		else{
+			//	else toss final ip
+			sendMessage=newMessage;
+			newMessage=newMessage.split("#")[1];
+		}
+		String [] message=newMessage.split(",");
+		String key=message[0];
+
+		boolean isMine=isMine(key);
+		if (isMine){
+			String answer=Server.action(newMessage);
+			//send back reply
+			sendMessage(sendMessage.split("#")[0], answer);
+		}
+		else{
+			
+			outNext.println(sendMessage);
+		}
+
+	}
 	
 	public void connectWithNext(String iPort) throws IOException{
 
@@ -94,6 +329,48 @@ public class RoutingServer extends Thread{
 		//open connection with next
 		socketNext = new Socket(next[0],new Integer(next[1]));
 		outNext = new PrintWriter(socketNext.getOutputStream(), true);
+	}
+	public void depart(String leaveMessage){
+		String message="Leaving-"+this.myShaId;
+		boolean success=sendMessage(oneIp,onePort,message);
+		if(success || leaveMessage.startsWith("LeaveForced"))
+			System.exit(0);
+	}
+	
+	private String reform(String[] message) {
+		/*
+		 * TODO read how many copies already done 
+		 * and reduce the amount of copies left
+		 */
+		return message.toString();
+	}
+
+	protected boolean isMine(String key) {
+		if (amiFirst){
+			return (  compareHash(start,key)<0  &&  compareHash(key,getMaxSha1())<0 )
+				|| (  compareHash(getZeroSha1(),key)<0  &&  compareHash(key,end)<0  ) ;
+		}
+		return compareHash(start,key)<0  && compareHash(key,end)<0 ;
+	}
+	
+	private String getZeroSha1() {
+		// TODO Auto-generated method stub
+		String zero="0000000000";	//10 
+		return zero+zero+zero+zero;
+	}
+
+	private String getMaxSha1() {
+		// TODO find decoding used for 2**160
+		return null;
+	}
+
+	protected int compareHash(String h1,String h2){
+		for(int i=0;i<40;i++){
+			int d=h1.charAt(i)-h2.charAt(i);
+			if(d!=0)
+			return d;
+		}
+		return 0;
 	}
 	
 	private boolean sendMessage(String ip, int port, String message){
@@ -118,121 +395,15 @@ public class RoutingServer extends Thread{
 		
 		
 	}
-	
-	private void listen(){
-		try{
-		srvSocket= new ServerSocket(myPort,7); //backlog (max queue)
-		System.out.println("Listening");
-		while(true){
-			Socket sock=srvSocket.accept();
-			System.out.println("Connected");
-			BufferedReader inPrev = new BufferedReader(
-	                new InputStreamReader(sock.getInputStream()));
-			String newMessage=inPrev.readLine();
-			System.out.println(newMessage);
-			
-			if(newMessage.startsWith("Leave")){
-				depart(newMessage);
-				// we may not manage to leave
-				continue;
-			}
-			
-			if (newMessage.startsWith("Answer-")){
-				//if an answer, comes just print it (?)
-				//	Answer-<answer>
-				System.out.println(newMessage.split("-")[1]);
-				continue;
-			}
-	
-			if(newMessage.startsWith("One-")){
-				//	Some change happened (someone left or came)
-				//  One-ipNext:portNext
-				connectWithNext(newMessage.split("-")[1]);
-				continue;
-			}
-			
-			if(newMessage.startsWith("NewHigh-")){
-				//New Range due to new Node in network
-				String[] ranges=newMessage.split("-");
-				end=Long.parseLong(ranges[1]);
-				continue;
-			}
-			
-			if(newMessage.startsWith("NewLow-")){
-				//New Range due to new Node in network
-				String[] ranges=newMessage.split("-");
-				start=Long.parseLong(ranges[1]);
-				continue;
-			}
-			
-			if(newMessage.startsWith("OneLeft-")){
-				//  TODO : One Died ?
-				continue;
-			}
-
-			String sendMessage;
-			if(!newMessage.contains("#")){
-				/* 
-				 * If this is the first hop
-				 * add my ip:port# to take Answer
-				 */
-				sendMessage=myIp+":"+myPort+"#"+newMessage;
-			}
-			else{
-				//	else toss final ip
-				sendMessage=newMessage;
-				newMessage=newMessage.split("#")[1];
-			}
-			String [] message=newMessage.split(",");
-			String key=message[0];
-
-			boolean isMine=isMine(key);
-			if (isMine){
-				String answer=Server.action(newMessage);
-				//send back reply
-				sendMessage(sendMessage.split("#")[0], answer);
-			}
-			else{
-				
-				outNext.println(sendMessage);
-			}
-		}
-	}
-		
-	catch(IOException e){
-		e.printStackTrace();
-	}
-	}
-	
-	public void depart(String leaveMessage){
-		String message="Leaving-"+this.myShaId;
-		boolean success=sendMessage(oneIp,onePort,message);
-		if(success || leaveMessage.startsWith("LeaveForced"))
-			System.exit(0);
-	}
-	
-	private String reform(String[] message) {
-		/*
-		 * TODO read how many copies already done 
-		 * and reduce the amount of copies left
-		 */
-		return message.toString();
-	}
-
-	private boolean isMine(String key) {
-		Long sha1=new Long(hash(key));
-		return (sha1>start && sha1<end);  
-	}
-
 	private String getKey(String message) {
 		return null;
 	}
 	
-	protected String hash(String s){
+	public static String hash(String s){
 		return org.apache.commons.codec.digest.DigestUtils.sha1Hex("");
 	}
 	
-	protected String hash(int n){
+	public static String hash(int n){
 		return hash(n+"");
 	}
 	
